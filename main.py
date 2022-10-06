159b4bbde7a72ac449f027cdeb6b27874e8ecbaf
Abhijit47 <akarmakar846@gmail.com>
2022-10-06 20:25:41 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print('🚀🔥Rocking Python🔥🚀')
+# First We take a input from the user
+inputValue = int(input("Enter a value to check Even or Odd: "))
+
+# Then we check the value is even or odd
+if (inputValue % 2 == 0):
+    # If the value is even then we print the value is even
+    print(f"{inputValue} is the value that you entered is Even")
+# If the value is odd then we print the value is odd
+else:
+    print(f"{inputValue} is the value that you entered is Odd")
+
+    # 🚀🚀🚀🚀 Very interesting to learn python🚀🚀🚀🚀 you are rocking Sir SaiKiran🚀🚀🚀🚀
